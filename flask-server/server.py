e233d8973baa5ce5e86ab1c2fd8363bdf50bc95e
mingw16 <asmierciak098@gmail.com>
2023-08-07 10:44:06 +0200

    setup connect with flask

--- flask-server/server.py
+++ flask-server/server.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/members")
 def members():
-    return {"members:":["member1", "member2", "member3"]}
+    return {"members":["member1", "member7", "member3"]}
 
 if __name__ == "__main__":
     app.run(debug=True)
